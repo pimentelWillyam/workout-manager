737714c87682aea307a34f484a8f925f3319a455
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:45:48 -0300

    feat: adding exercise test screen

---
+++ src/screens/ExerciseTestScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ExerciseLevelTestScreen() {
+  return (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Teste de nível</Text>
+  </View>
+  );
+}
