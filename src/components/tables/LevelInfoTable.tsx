8fa179e5e240fd15ec7eb686176ca1a1764e163c
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:45:25 -0300

    feat: adding level info table

---
+++ src/components/tables/LevelInfoTable.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NativeBaseProvider, Box, Checkbox } from "native-base";
+
+export default function LevelInfoTable() {
+  return (
+    <table>
+      aaa
+    </table>
+  )
+}
