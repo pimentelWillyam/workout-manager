c397a05d6a64108a7d0866ad6d40f2a6a2f5ea19
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:45:38 -0300

    feat: adding exercise level info screen

---
+++ src/screens/ExerciseLevelInfoScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+export default function ExerciseLevelInfoScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Nivel de exercício</Text>
+  </View>
+  );
+}
