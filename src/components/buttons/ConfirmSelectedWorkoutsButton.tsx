5c478e84c93182efa3e8f8d7569d488c8edaf9c9
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:28:22 -0300

    feat: adding confirm selection button

---
+++ src/components/buttons/ConfirmSelectedWorkoutsButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NativeBaseProvider, Box, Button } from "native-base";
+
+export default function ConfirmSelectedWorkoutsButton() {
+  return (
+        <Button>Confirmar</Button>
+  );
+}
