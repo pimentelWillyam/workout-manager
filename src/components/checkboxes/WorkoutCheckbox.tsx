c8bc20419e58086aabe4f08cfdea01ae96b9c8cc
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:44:48 -0300

    feat: adding workout checkbox

---
+++ src/components/checkboxes/WorkoutCheckbox.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NativeBaseProvider, Box, Checkbox } from "native-base";
+
+export default function WorkoutCheckbox() {
+  return (
+      <Checkbox > Workout Checkbox</Checkbox>
+  );
+}
