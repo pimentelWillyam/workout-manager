96bdd88cb8e32cc0cf745a5e4456f92665f32a18
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-30 16:45:13 -0300

    feat: adding maximum repeat form

---
+++ src/components/forms/MaximumRepeatForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NativeBaseProvider, Box, Checkbox } from "native-base";
+
+export default function MaximumRepeatForm() {
+  return (
+    <NativeBaseProvider>
+      <FormData> Maximum Repeat</FormData>
+    </NativeBaseProvider>
+  );
+}
