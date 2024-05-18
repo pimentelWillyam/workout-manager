39610d7ab1d9788cdac9c1d011ecfb68f59b00e2
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 12:45:42 -0300

    feat: adding situps screen

---
+++ src/front/screens/SitupsScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "native-base"
+import { SitupsBox } from '../components/boxes/SitupsBox';
+import React from "react";
+
+export const SitupsScreen = () => {
+  return(
+    <View>
+        <SitupsBox />
+    </View>
+  )
+}
