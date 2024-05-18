11b74369eff53dec5e50a5b8e1d442a8d8c10ee2
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 12:49:38 -0300

    feat: adding exercises to workout at workout screen

--- src/front/screens/WorkoutsScreen.tsx
+++ src/front/screens/WorkoutsScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ExercisesToWorkoutMenu from "../components/menus/ExercisesToWorkoutMenu";
+
 
 export default function WorkoutsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Exercício</Text>
+    <ExercisesToWorkoutMenu />
   </View>
   );
 }
