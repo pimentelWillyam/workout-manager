771cba0753165fcf5fedbfc416472cd2a82c6ed2
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 12:45:35 -0300

    feat: adding pushups screen

---
+++ src/front/screens/PushupsScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "native-base"
+import React from "react"
+import { PushupsBox } from '../components/boxes/PushupsBox';
+
+export const PushupsScreen = () => {
+  return(
+    <View>
+      <PushupsBox />
+    </View>
+  )
+}
