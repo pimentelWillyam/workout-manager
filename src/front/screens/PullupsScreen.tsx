1fe9f9352723c39174931b13b0aaac3b848ffe76
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 12:45:25 -0300

    feat: adding pullup screen

---
+++ src/front/screens/PullupsScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "native-base"
+import React from "react"
+import { PulllupsBox } from "../components/boxes/PulllupsBox";
+
+export const PullupsScreen = () => {
+  return(
+    <View>
+      <PulllupsBox />
+    </View>
+  )
+}
