428fde2c4795ffff5b10408fbb2c3f1690f7858b
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 09:55:44 -0300

    feat: adding selected workouts list component

--- src/front/components/lists/SelectedWorkoutsList.tsx
+++ src/front/components/lists/SelectedWorkoutsList.tsx
@@ -1,17 +1,25 @@
 import React from "react"
-import { WorkoutList } from "../../../utils/WorkoutList"
+import { WorkoutListFetcher } from "../../../utils/fetchers/WorkoutListFetcher"
 import { FlatList, Text } from "react-native"
 import { VStack, Center } from "native-base";
+import { useEffect, useState } from 'react';
 export default function SelectedWorkoutsList() {
+
+  const componentsToRenderAtTheVStack: React.JSX.Element[] = []
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const workoutList = WorkoutListFetcher.execute()
+      workoutList.map((workout) => componentsToRenderAtTheVStack.push(<Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3}><Text>{workout.name}</Text></Center>))
+    }
+    fetchWorkouts()
+  }, []); // O array vazio como segundo argumento faz o useEffect ser executado apenas uma vez, quando o componente é montado.
+
+
   return (
-      <FlatList 
-      data={WorkoutList.get()}
-      renderItem={((workout) => <Center padding={"10"} w="64" h="20" bg="indigo.700" rounded="md" shadow={3}><Text>{workout.item.name}</Text></Center>)}
-      >
-      </FlatList>
-      // <VStack space={4} alignItems="center">
-      //   <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3}/>
-      // </VStack>
+    <VStack space={4} alignItems="center">
+      {componentsToRenderAtTheVStack}
+    </VStack>
   )
 
 
