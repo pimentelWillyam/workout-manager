8430bc7a2c897de85a7424aa3a69a14125dd14d5
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-18 12:45:50 -0300

    feat: adding squats screen

---
+++ src/front/screens/SquatsScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "native-base"
+import { SquatsBox } from "../components/boxes/SquatsBox"
+import React from "react"
+
+export const SquatsScreen = () => {
+  return(
+    <View>
+      <SquatsBox />
+    </View>
+  )
+}
