61a85895ff4aa1da99a057a03cc23f6a4f167140
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-05-25 17:11:44 -0300

    chore: adding async storage dependencies

--- src/back/datasources/UserDataSource.ts
+++ src/back/datasources/UserDataSource.ts
@@ -8,25 +8,25 @@ export class UserDataSource {
   private static database: Database
 
   static async insertUserRegistry(user: User): Promise<void> {
-    const result = await this.database.getConnection().executeSql(SQLiteQueries.insertUserRegistry, [user.name, user.current_pushup_level, user.current_pullup_level, user.current_situp_level, user.current_squat_level, user.tracking_pushups, user.tracking_pullups, user.tracking_situps, user.tracking_squats])
+    // const result = await this.database.getConnection().executeSql(SQLiteQueries.insertUserRegistry, [user.name, user.current_pushup_level, user.current_pullup_level, user.current_situp_level, user.current_squat_level, user.tracking_pushups, user.tracking_pullups, user.tracking_situps, user.tracking_squats])
   }
 
   static async fetchEveryUserRegistry(): Promise<void> {
-    await this.database.getConnection().executeSql(SQLiteQueries.fetchEveryUserRegistry)
+    // await this.database.getConnection().executeSql(SQLiteQueries.fetchEveryUserRegistry)
 
   }
 
   static async fetchUserRegistry(id: number): Promise<void> {
-    const result = await this.database.getConnection().executeSql(SQLiteQueries.fetchUserById, [id])
+    // const result = await this.database.getConnection().executeSql(SQLiteQueries.fetchUserById, [id])
   }
 
   static async updateUserRegistry(id: number, upToDateUser: User): Promise<void> {
-    this.database.getConnection().executeSql(SQLiteQueries.updateUserRegistry, [upToDateUser.name, upToDateUser.current_pushup_level, upToDateUser.current_pullup_level, upToDateUser.current_situp_level, upToDateUser.current_squat_level, upToDateUser.tracking_pushups, upToDateUser.tracking_pullups, upToDateUser.tracking_situps, upToDateUser.tracking_squats, id])
+    // this.database.getConnection().executeSql(SQLiteQueries.updateUserRegistry, [upToDateUser.name, upToDateUser.current_pushup_level, upToDateUser.current_pullup_level, upToDateUser.current_situp_level, upToDateUser.current_squat_level, upToDateUser.tracking_pushups, upToDateUser.tracking_pullups, upToDateUser.tracking_situps, upToDateUser.tracking_squats, id])
 
   }
 
   static async deleteUserRegistry(id: number): Promise<void> {
-    const result = await this.database.getConnection().executeSql(SQLiteQueries.deleteUserRegistry, [id])
+    // const result = await this.database.getConnection().executeSql(SQLiteQueries.deleteUserRegistry, [id])
   }
 
 
